7241b3a6d4834c1a831d3267db12cb644f34354d
manzeela ferdows <manzeela.f@hotmail.co.uk>
2021-02-20 15:11:21 +0000

    Recap of collections

---
+++ collections.py
@@ -0,0 +1,60 @@
+str = 'Norwegian Blue', "Mr Khan's Bike"
+list = ['cheddar', ['Camembert', 'Brie'], 'Stilton', 'Brie', 'Brie']
+tuples = (47, 'spam','Major', 638, 'Ovine Aviation')
+tuples2 = (36, 29, 63)
+set = {'cheeseburger', 'icecream', 'chicken nuggets'}
+dict = {'Totness' : 'Barber', 'BritishColumbia' : 'Lumberjack'}
+
+print(len(list))
+print(min(tuples2))
+print(max(tuples2))
+print(sum(tuples2))
+
+a = 'Mash Potato'
+b = 'Gravy'
+
+print(a,b)
+a,b = b, a
+print(a,b)
+
+Gouda, Edam, Caithness = range(3)
+print(range)
+
+Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
+Belgium2 = Belgium*2
+print(Belgium2)
+
+thing = ('Hello')
+print(type(thing))
+thing = ('Hello',)
+print(type(thing))
+
+list[:0] = ['Newham', 'Hackney']
+print(list)
+
+list += ['Southwark', 'Barking']
+print(list)
+
+list.extend(['Dagenham', 'Croydon'])
+print(list)
+
+list.insert(5, 'Tottenham')
+print(list)
+
+list[5:5] = ['Edmonton']
+print(list)
+
+list2 = list.pop(1)
+print(list2)
+
+list3 = list.pop(3)
+print('the two items we removed:', list2, 'and', list3, "Now the list is shorter:", list)
+
+list.remove('Edmonton')
+print(list)
+
+print(list.count('Brie'))
+print(list.index('Brie'))
+
+list.reverse()
+print(list)
